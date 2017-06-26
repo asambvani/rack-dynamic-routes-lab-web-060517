bdc8fcf1963df913313c9811ae98b86f1b0ae4b3
asambvani <alexsambvani@gmail.com>
2017-06-26 13:27:00 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+require_relative "item.rb"
+
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/").last
+      item = Item.find_by_name(item_name)
+      if item != nil
+        price = item.price
+        resp.write price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,23 @@
 class Item
   attr_accessor :name, :price
 
+  @@all = []
+
   def initialize(name,price)
     @name = name
     @price = price
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    puts "THE NAME IS #{name}"
+    self.all.select do |item|
+      item.name == name
+    end.first
+  end
+
 end
